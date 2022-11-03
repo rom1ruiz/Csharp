2141838265cc272f3142d6c65428ec2a2c7379a6
Formation <Formation@DESKTOP-AN89IF0>
2022-11-03 12:49:39 +0100

    Day 1 and half

---
+++ Serie II/Ex1_Search.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_II
+{
+    public static class Search
+    {
+        public static int LinearSearch(int[] tableau, int valeur)
+        {
+            
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] == valeur)
+                {
+
+                    
+                    return i;
+                }
+
+
+            }
+            return -1;
+        }
+
+        public static int BinarySearch(int[] tableau, int valeur)
+        {
+            int mid = (tableau.Length / 2);
+            int deb = 0;
+            int fin = tableau.Length;
+            while (deb != fin)
+            {
+                if (tableau[mid] == valeur)
+                {
+                    return mid;
+                }
+                else if (tableau[mid] > valeur)
+                {
+                    fin = tableau[mid];
+
+                }
+                
+
+                
+            }
+            return -1;
+        }
+    }
+}
---
+++ Serie II/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valeur = 12;
+            int[] tab = new int[] { -3, 2, 3, 4, 5, 8, 9, 12 };
+            Console.WriteLine($"LinearSearch : { Search.LinearSearch(tab, valeur)}");
+            Array.Sort(tab);
+            Console.WriteLine($"BinarySearch : {Search.BinarySearch(tab,valeur)}");
+            Search.BinarySearch(tab, valeur);
+            // Keep the console window open
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
